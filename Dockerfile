bfa8487bb68b6a6224126e1a1960cd80b2f6ead1
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-06-09 12:41:17 +0200

    Initial commit after tweaking fork to work for me

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM alpine:edge
-MAINTAINER Tobias Gesellchen <tobias@gesellix.de> (@gesellix)
+MAINTAINER Mathias Klippinge <mathias.klippinge@gmail.com> (@klippx)
 
 RUN apk add -U openssl
 
-ENV DOMAIN example.com
-ENV PORT 5000
+ENV CERTIFICATE example.crt
 
 CMD ["/entrypoint.sh"]
 
 COPY entrypoint.sh /entrypoint.sh
-COPY restart-docker.sh /restart-docker.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-mkdir -p /vm-etc/docker/certs.d/${DOMAIN}:${PORT}
-rm -rf /vm-etc/docker/certs.d/${DOMAIN}:${PORT}/*
-
-cp /certs/${DOMAIN}:${PORT}/cert.cert /vm-etc/docker/certs.d/${DOMAIN}:${PORT}/
-cp /certs/${DOMAIN}:${PORT}/cert.key /vm-etc/docker/certs.d/${DOMAIN}:${PORT}/
-
-openssl verify -verbose -CAfile /vm-etc/ssl/certs/ca-certificates.crt /vm-etc/docker/certs.d/${DOMAIN}:${PORT}/cert.cert
-CA_CERT_EXISTS=$?
-if [ $CA_CERT_EXISTS -ne 0 ]; then
-	echo "adding ca.cert to /vm-etc/ssl/certs/ca-certificates.crt"
-	cat /certs/${DOMAIN}:${PORT}/ca.cert >> /vm-etc/ssl/certs/ca-certificates.crt
-fi
-
-# cp /restart-docker.sh /vm-etc/periodic/15min/restart-docker
-# echo "rm /etc/periodic/15min/restart-docker" >> /vm-etc/periodic/15min/restart-docker
+echo "adding ${CERTIFICATE} to /vm-etc/ssl/certs/ca-certificates.crt"
+cat /certs/${CERTIFICATE} >> /vm-etc/ssl/certs/ca-certificates.crt
