e7818e5eca4b14fac98a3e678ddaa8bdd3a10353
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-06-10 08:35:21 +0200

    Keep it simple

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 openssl verify -verbose -CAfile /vm-etc/ssl/certs/ca-certificates.crt /certs/${CERTIFICATE}
-CA_CERT_EXISTS=$?
-if [ $CA_CERT_EXISTS -ne 0 ]; then
-  echo "adding ${CERTIFICATE} to /vm-etc/ssl/certs/ca-certificates.crt"
-  cat /certs/${CERTIFICATE} >> /vm-etc/ssl/certs/ca-certificates.crt
-else
-  echo "${CERTIFICATE} is already added to VM CAs"
-fi
+
+echo "adding ${CERTIFICATE} to /vm-etc/ssl/certs/ca-certificates.crt"
+cat /certs/${CERTIFICATE} >> /vm-etc/ssl/certs/ca-certificates.crt
